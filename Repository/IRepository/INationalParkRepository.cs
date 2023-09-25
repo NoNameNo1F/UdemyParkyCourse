9afd2678bd1ac2ab1c5e9abe86cc7652351245f3
20127670 <nonameno1f@gmail.com>
2023-09-25 15:46:26 +0700

    Section 3: Add IRepository Pattern

---
+++ Repository/IRepository/INationalParkRepository.cs
@@ -0,0 +1,16 @@
+using PankyAPI.Models;
+
+namespace PankyAPI.Repository.IRepository
+{
+    public interface INationalParkRepository
+    {
+        ICollection<NationalPark> GetNationalParks();
+        NationalPark GetNationalPark(int nationalParkId);
+        bool NationalParkExists(string name);
+        bool NationalParkExists(int id);
+        bool CreateNationalPark(NationalPark nationalPark);
+        bool UpdateNationalPark(NationalPark nationalPark);
+        bool DeleteNationalPark(NationalPark nationalPark);
+        bool Save();
+    }
+}
