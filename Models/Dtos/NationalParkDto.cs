c1e604ade2ce8660ddf38bed934eb4048a2a1880
20127670 <nonameno1f@gmail.com>
2023-09-25 17:23:06 +0700

    Add Dto

---
+++ Models/Dtos/NationalParkDto.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace PankyAPI.Models.Dtos
+{
+    public class NationalParkDto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        [Required]
+        public string? State { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public byte[]? Picture { get; set; }
+        public DateTime Established { get; set; }
+    }
+}
